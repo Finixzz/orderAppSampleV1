5300dc5aaab41e0cb632b7fef44b541c0bbbd29e
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-07 11:00:29 +0100

    Added OrderDto and OrderContentDto

---
+++ orderAppSampleV1/Dtos/OrderContentDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace orderAppSampleV1.Dtos
+{
+    public class OrderContentDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public List<int> ItemId { get; set; }
+
+        [Required]
+        public int TableId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public List<int> Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ orderAppSampleV1/Dtos/OrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace orderAppSampleV1.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+
+        public int ItemId { get; set; }
+
+        public int TableId { get; set; }
+
+        public string UserId { get; set; }
+
+        public byte Quantity { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
