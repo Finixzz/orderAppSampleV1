32e48d3a08693cdd4b2395c59d35c673c79c0cfc
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-02-07 11:19:16 +0100

    Added ordersController

---
+++ orderAppSampleV1/Api/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using orderAppSampleV1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace orderAppSampleV1.Api
+{
+    public class OrdersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public OrdersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult GetOrders()
+        {
+
+            return Ok();
+        }
+    }
+}
